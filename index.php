9eb77a041b12f78c896de812138889fddcf53451
Lukas Mareda <lukas.mareda@novartis.com>
2019-08-29 14:14:03 +0200

    as

--- index.php
+++ index.php
@@ -15,13 +15,13 @@ try {
     $pdo = new PDO($dsn);
 
 	$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );//Error Handling
-    $sql ="CREATE TABLE banks (ID serial PRIMARY KEY, Name VARCHAR(50) NOT NULL, Created timestamp);" ;
+//    $sql ="CREATE TABLE banks (ID serial PRIMARY KEY, Name VARCHAR(50) NOT NULL, Created timestamp);" ;
+//    $pdo->exec($sql);
+    
+//    $sql ="CREATE TABLE users(ID serial PRIMARY KEY, Name VARCHAR(50) NOT NULL, Role integer NOT NULL, Created timestamp);" ;
     $pdo->exec($sql);
     
-    $sql ="CREATE TABLE users(ID serial PRIMARY KEY, Name VARCHAR(50) NOT NULL, Role integer NOT NULL, Created timestamp);" ;
-    $pdo->exec($sql);
-    
-    $sql ="CREATE TABLE transactions (ID serial PRIMARY KEY, 'UID' integer NOT NULL, 'BID' integer NOT NULL, 'Grain' integer NOT NULL, 'Wood' integer NOT NULL, 'Stone' integer NOT NULL, 'Iron' integer NOT NULL, 'Gold' integer NOT NULL, 'Created' timestamp);";
+    $sql ="CREATE TABLE transactions (ID serial PRIMARY KEY, UID integer NOT NULL, BID integer NOT NULL, Grain integer NOT NULL, Wood integer NOT NULL, Stone integer NOT NULL, Iron integer NOT NULL, Gold integer NOT NULL, Created timestamp);";
     $pdo->exec($sql);
     
     
