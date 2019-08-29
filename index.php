50dba2755ca5b79be1bc21d38ef241bf7cb7bc87
Lukas Mareda <lukas.mareda@novartis.com>
2019-08-29 12:56:24 +0200

    updates

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<?php 
+
+$db = parse_url(getenv("DATABASE_URL"));
+
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["ec2-54-217-235-87.eu-west-1.compute.amazonaws.com"],
+    $db["5432"],
+    $db["zhtzkjcvxxjmfk"],
+    $db["0d089aae8342db654d90cb7cb2e652bdf303918af61f9d997f380a80aba204f0"],
+    ltrim($db["d4es7alaadvmih"], "/")
+));
+
+echo($pdo);
+
+?>
