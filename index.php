565cff27d7c804c17f238e0951326d01c09236a7
Lukas Mareda <lukas.mareda@novartis.com>
2019-08-29 14:15:36 +0200

    as

--- index.php
+++ index.php
@@ -8,8 +8,6 @@ $dbname = 'd4es7alaadvmih';
 $username = 'zhtzkjcvxxjmfk';
 $password = '0d089aae8342db654d90cb7cb2e652bdf303918af61f9d997f380a80aba204f0';
 $dsn = "pgsql:host=$host;port=5432;dbname=$dbname;user=$username;password=$password";
-
-    echo $dsn;
     
 try {
     $pdo = new PDO($dsn);
@@ -19,7 +17,7 @@ try {
 //    $pdo->exec($sql);
     
 //    $sql ="CREATE TABLE users(ID serial PRIMARY KEY, Name VARCHAR(50) NOT NULL, Role integer NOT NULL, Created timestamp);" ;
-    $pdo->exec($sql);
+//    $pdo->exec($sql);
     
     $sql ="CREATE TABLE transactions (ID serial PRIMARY KEY, UID integer NOT NULL, BID integer NOT NULL, Grain integer NOT NULL, Wood integer NOT NULL, Stone integer NOT NULL, Iron integer NOT NULL, Gold integer NOT NULL, Created timestamp);";
     $pdo->exec($sql);
