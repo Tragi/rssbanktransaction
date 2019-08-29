c5debd420ce0328edd120dc0d46a6e316b521efd
Lukas Mareda <lukas.mareda@novartis.com>
2019-08-29 21:40:08 +0200

    asd

--- list.php
+++ list.php
@@ -4,7 +4,7 @@
             <th scope="col">Datum</th>
             <th scope="col">Grain</th>
             <th scope="col">Wood</th>
-            <th scope="col">Strone</th>
+            <th scope="col">Stone</th>
             <th scope="col">Iron</th>
             <th scope="col">Gold</th>
         </tr>
