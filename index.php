2f7a664f84f8cb2caf92124cae9d6eb6f3d62add
Lukas Mareda <lukas.mareda@novartis.com>
2019-08-29 13:56:10 +0200

    update

--- index.php
+++ index.php
@@ -15,25 +15,12 @@ try {
     $pdo = new PDO($dsn);
 
 	$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );//Error Handling
-    $sql ="CREATE TABLE employees (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    firstname VARCHAR(30) NOT NULL,
-    lastname VARCHAR(30) NOT NULL,
-    email VARCHAR(50),
-    reg_date TIMESTAMP
-    )";
+	
+    $sql ="CREATE table transactions ( ID INT(11) AUTO_INCREMENT PRIMARY KEY, UID INT(11) NOT NULL, BID INT(11) NOT NULL, Grain INT(11) NOT NULL, Wood INT(11) NOT NULL, Stone INT(11) NOT NULL, Iron INT(11) NOT NULL, Gold INT(11) NOT NULL, Created TIMESTAMP NOT NULL DEFAULT NOW();";
     $pdo->exec($sql);
-    $sql ="CREATE table users(
-        ID INT( 11 ) AUTO_INCREMENT PRIMARY KEY,
-        Name VARCHAR( 50 ) NOT NULL,
-        Rrole INT ( 11 ) NOT NULL,
-        Created TIMESTAMP NOT NULL DEFAULT NOW();" ;
+    $sql ="CREATE table users( ID INT( 11 ) AUTO_INCREMENT PRIMARY KEY, Name VARCHAR( 50 ) NOT NULL, Role INT ( 11 ) NOT NULL, Created TIMESTAMP NOT NULL DEFAULT NOW();" ;
     $pdo->exec($sql);
-
-    $sql ="CREATE table banks(
-    ID INT( 11 ) AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR( 50 ) NOT NULL,
-    Created TIMESTAMP NOT NULL DEFAULT NOW();" ;
+    $sql ="CREATE table banks( ID INT( 11 ) AUTO_INCREMENT PRIMARY KEY, Name VARCHAR( 50 ) NOT NULL, Created TIMESTAMP NOT NULL DEFAULT NOW();" ;
     $pdo->exec($sql);
 
     
