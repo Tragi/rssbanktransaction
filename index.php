918417711ead4b00163c50077c7812a0a0412946
Lukas Mareda <lukas.mareda@novartis.com>
2019-08-30 10:08:46 +0200

    asd

--- index.php
+++ index.php
@@ -13,10 +13,10 @@ function echoTransactions() {
     global $pdo;
     $stmt = $pdo->prepare("SELECT * FROM transactions WHERE uid = :id");
     $stmt->bindValue(':id', 1);
-    //        $stmt->execute();
-    //        while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
-    //            var_dump($row)
-    //        }
+    $stmt->execute();
+    while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+        var_dump($row)
+    }
 }
     
 try {
