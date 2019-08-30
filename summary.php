6ddf4095988f0cd5d068c13c9d72c9a6bbee874d
Lukas Mareda <lukas.mareda@novartis.com>
2019-08-30 19:30:34 +0200

    no message

--- summary.php
+++ summary.php
@@ -29,11 +29,11 @@
             $stone = number_format($row["stone"], 0, ',', ' ');
             $iron = number_format($row["iron"], 0, ',', ' ');
             $gold = number_format($row["gold"], 0, ',', ' ');
-            $grainClass = $row["grain"] > 0 ? "table-success" : ($row["grain"] == 0 ? "table-info" : "table-danger");
-            $woodClass = $row["wood"] > 0 ? "table-success" : ($row["wood"] == 0 ? "table-info" : "table-danger");
-            $stoneClass = $row["stone"] > 0 ? "table-success" : ($row["stone"] == 0 ? "table-info" : "table-danger");
-            $ironClass = $row["iron"] > 0 ? "table-success" : ($row["iron"] == 0 ? "table-info" : "table-danger");
-            $goldClass = $row["grain"] > 0 ? "table-success" : ($row["grain"] == 0 ? "table-info" : "table-danger");
+            $grainClass = $row["grain"] > 0 ? "table-success" : ($row["grain"] == 0 ? "table-light" : "table-danger");
+            $woodClass = $row["wood"] > 0 ? "table-success" : ($row["wood"] == 0 ? "table-light" : "table-danger");
+            $stoneClass = $row["stone"] > 0 ? "table-success" : ($row["stone"] == 0 ? "table-light" : "table-danger");
+            $ironClass = $row["iron"] > 0 ? "table-success" : ($row["iron"] == 0 ? "table-light" : "table-danger");
+            $goldClass = $row["gold"] > 0 ? "table-success" : ($row["gold"] == 0 ? "table-light" : "table-danger");
             echo "<tr class='table-'><td style=\"width:100px\">$name</td><td class=\"$grainClass\" style=\"width:120px\">$grain</td><td class=\"$woodClass\" style=\"width:120px\">$wood</td><td class=\"$stoneClass\" style=\"width:120px\">$stone</td><td class=\"$ironClass\" style=\"width:120px\">$iron</td><td class=\"$goldClass\" style=\"width:120px\">$gold</td></tr>";
         }
         $grain = number_format($sumGrain, 0, ',', ' ');
@@ -41,12 +41,12 @@
         $stone = number_format($sumStone, 0, ',', ' ');
         $iron = number_format($sumIron, 0, ',', ' ');
         $gold = number_format($sumGold, 0, ',', ' ');
-        $grainClass = $sumGrain > 0 ? "table-success" : ($sumGrain == 0 ? "table-info" : "table-danger");
-        $woodClass = $sumWood > 0 ? "table-success" : ($sumWood == 0 ? "table-info" : "table-danger");
-        $stoneClass = $sumStone > 0 ? "table-success" : ($sumStone == 0 ? "table-info" : "table-danger");
-        $ironClass = $sumIron > 0 ? "table-success" : ($sumIron == 0 ? "table-info" : "table-danger");
-        $goldClass = $sumGold > 0 ? "table-success" : ($sumGold == 0 ? "table-info" : "table-danger");
-        return "<tr class='table-primary'><td scope=\"col\" style=\"width:100px\">Součet</td><td class=\"$grainClass\" scope=\"col\" style=\"width:120px\">$grain</td><td class=\"$woodClass\" scope=\"col\" style=\"width:120px\">$wood</td><td class=\"$stoneClass\" scope=\"col\" style=\"width:120px\">$stone</td><td class=\"$ironClass\" scope=\"col\" style=\"width:120px\">$iron</td><td class=\"$goldClass\" scope=\"col\" style=\"width:120px\">$gold</td></tr>";
+        $grainClass = $sumGrain > 0 ? "table-success" : ($sumGrain == 0 ? "table-light" : "table-danger");
+        $woodClass = $sumWood > 0 ? "table-success" : ($sumWood == 0 ? "table-light" : "table-danger");
+        $stoneClass = $sumStone > 0 ? "table-success" : ($sumStone == 0 ? "table-light" : "table-danger");
+        $ironClass = $sumIron > 0 ? "table-success" : ($sumIron == 0 ? "table-light" : "table-danger");
+        $goldClass = $sumGold > 0 ? "table-success" : ($sumGold == 0 ? "table-light" : "table-danger");
+        return "<tr><td class='table-primary' scope=\"col\" style=\"width:100px\">Součet</td><td class=\"$grainClass\" scope=\"col\" style=\"width:120px\">$grain</td><td class=\"$woodClass\" scope=\"col\" style=\"width:120px\">$wood</td><td class=\"$stoneClass\" scope=\"col\" style=\"width:120px\">$stone</td><td class=\"$ironClass\" scope=\"col\" style=\"width:120px\">$iron</td><td class=\"$goldClass\" scope=\"col\" style=\"width:120px\">$gold</td></tr>";
     }
     
     
