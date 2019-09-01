369a95fdce2483cab9f6e8dcf11d2b48b7e3f975
Lukas Mareda <lukas.mareda@novartis.com>
2019-09-01 15:24:59 +0200

    asd

--- profileTemplate.php
+++ profileTemplate.php
@@ -7,22 +7,34 @@
                 <div class="p2 col-4">
                     <h2>Profil</h2>
                     <div class="">
-                        <form id="transactionForm" method="post" action="/." onsubmit="return false;">
-                            <label for="customRange2">Gold</label>
+                        <form id="armyForm" method="post" action="/." onsubmit="return false;">
+                            <label for="customRange2">T3</label>
                             <div class="form-group row">
                                 <div class="d-flex flex-fill">
                                     <div class="col-8">
-                                        <input type="range" class="custom-range align-middle" min="0" max="10000000" step="100000" value="0"  id="goldSlider" />
+<input type="range" class="custom-range align-middle" min="0" max="10000000" step="10000" value=<?php echo "\"".army("t3")."\"";  ?>  id="t3Slider" />
                                     </div>
                                     <div class="col-4">
-                                        <input class="form-control" type="number" min="0" max="10000000" step="100000" value="0" id="goldInput" name="gold" />
+                                        <input class="form-control" type="number" min="0" max="10000000" step="10000" value=<?php echo "\"".army("t3")."\"";  ?> id="t3Input" name="t3" />
                                     </div>
                                 </div>
                             </div>
+                            <label for="customRange2">T4</label>
+                            <div class="form-group row">
+                                <div class="d-flex flex-fill">
+                                    <div class="col-8">
+                                        <input type="range" class="custom-range align-middle" min="0" max="10000000" step="10000" value=<?php echo "\"".army("t4")."\"";  ?>  id="t4Slider" />
+                                    </div>
+                                    <div class="col-4">
+                                        <input class="form-control" type="number" min="0" max="10000000" step="10000" value=<?php echo "\"".army("t3")."\"";  ?> id="t4Input" name="t4" />
+                                    </div>
+                                </div>
+                            </div>
+
                             <br />
                             <div class="form-group row">
                                 <div class="d-flex justify-content-end col-12">
-                                    <button id="submit" class="btn btn-primary col-3" data-dismiss="modal">Aktualizovat</button>
+                                    <button id="submit" class="btn btn-primary col-4" data-dismiss="modal">Aktualizovat</button>
                                 </div>
                             </div>
                         </form>
