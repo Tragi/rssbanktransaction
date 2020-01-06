0fefd5ceb0d33c9f8a5dc9aa73f353ae81f0917f
Lukas Mareda <lmareda@seznam.cz>
2020-01-06 21:05:36 +0100

    update

---
+++ asdaoshbfdksaljdbasjkdbas.php
@@ -0,0 +1,42 @@
+<?php
+    include("common.php");
+  
+    try {
+        $pdo = new PDO($dsn);
+        
+        $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );//Error Handling
+        //    $sql ="CREATE TABLE banks (ID serial PRIMARY KEY, Name VARCHAR(50) NOT NULL, Created timestamp);" ;
+        //    $pdo->exec($sql);
+
+$sql ="INSERT INTO banks (ID, Name, Created);" ;
+$stmt = $pdo->prepare("INSERT INTO banks (ID, Name, Created) VALUES (:id, :name, :created, :type)");
+        $stmt->bindParam(':id', 2);
+        $stmt->bindParam(':name', "Krtek");
+        $stmt->bindValue(':created', date('Y-m-d H:i:s'));
+        $stmt->execute();
+        
+        //    $sql ="CREATE TABLE users(ID serial PRIMARY KEY, Name VARCHAR(50) NOT NULL, Role integer NOT NULL, Created timestamp);" ;
+        //    $pdo->exec($sql);
+        
+        //    $sql ="CREATE TABLE transactions (ID serial PRIMARY KEY, UID integer NOT NULL, BID integer NOT NULL, Grain integer NOT NULL, Wood integer NOT NULL, Stone integer NOT NULL, Iron integer NOT NULL, Gold integer NOT NULL, Created timestamp);";
+        //    $pdo->exec($sql);
+        
+        //    var_dump($_POST);
+        //    if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') != 0){
+        //        throw new Exception('Request method must be POST!');
+        //    }
+        
+//        $sql ="DELETE FROM transactions;";
+//        $pdo->exec($sql);
+//        
+//        $sql ="DELETE FROM users;";
+//        $pdo->exec($sql);
+        
+        
+        include("summaryTemplate.php");
+        
+    } catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+    
+?>
